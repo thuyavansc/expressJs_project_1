129bbaa786d7cf4a614afb2c46d7d7494e0e0191
Kingston <Kingston@DESKTOP-MVP19NT>
2024-04-14 19:33:21 +0530

    req params

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,35 @@ const app = Express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUser = [
+  { id: 1, username: "anson", displayName: "anson" },
+  { id: 2, username: "jack", displayName: "anson" },
+  { id: 3, username: "adam", displayName: "anson" },
+];
+
 app.get("/", (request, response) => {
   response.send("Hello, World!").status(200);
 });
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 1, username: "anson", displayName: "anson" },
-    { id: 2, username: "jack", displayName: "anson" },
-    { id: 3, username: "adam", displayName: "anson" },
-  ]);
+  res.send(mockUser);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  console.log(req.params);
+
+  const parseId = parseInt(req.params.id);
+  console.log(parseId);
+
+  if (isNaN(parseId)) {
+    return res.status(400).send({
+      msg: "Bad Request. Invalid Number",
+    });
+  }
+
+  const findUser = mockUser.find((user) => user.id == parseId);
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
 });
 
 app.get("/api/products", (req, res) => {
